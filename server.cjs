152554b57af8dc03a0f1a62d1f550de9f197da7d
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-19 17:24:06 +0700

    feat: run local server api

--- server.cjs
+++ server.cjs
@@ -1,12 +1,29 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
-app.use(express.static('./'));
+app.use("static", express.static(__dirname));
+
+const API_PREFIX = "/api/v1";
+
+// Если мы сделаем POST-запрос сюда, то получим верный ответ
+// Если отправим GET-запрос, то получим либо 405 HTTP ошибку, либо 404
+app.get(`${API_PREFIX}/text`, (req, res) => {
+  res.status(200).send("Hello, World!");
+});
+
+app.put(`${API_PREFIX}/json`, (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.status(201).send({
+    data: {
+      items: [1, 2, 3],
+    },
+  });
+});
 
 app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
-}); 
+  console.log(`API RUN LOCALHOST:${PORT}!`);
+});
