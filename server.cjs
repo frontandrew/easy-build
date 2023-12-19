a90b0cdf3ed336be58078a3023d733732deb50d2
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-19 15:16:55 +0700

    feat: node server add

---
+++ server.cjs
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.static('./'));
+
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
+}); 
