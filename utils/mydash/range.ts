a9275601d4f96272b530668167025b77f08d6c6c
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-21 15:56:24 +0700

    feat: clear

--- utils/mydash/range.ts
+++ utils/mydash/range.ts
@@ -6,12 +6,9 @@ interface RangeConfig {
 
 export function range(start?: number, end: number, step?: number): number[] {
 
-    console.log(`PARAMS: { start: ${start}, end: ${end}, step: ${step} }`)
-
     const conf: RangeConfig = { start: 0, end: 0, step: 1 };
 
     const args: Array<number> = [start, end, step].filter(item => item === 0 ? true : Boolean(item));
-    console.log(`ARG [length ${args.length}]:`, args)
 
     if (args.length === 1) {
         conf.end = args[0]
@@ -27,10 +24,8 @@ export function range(start?: number, end: number, step?: number): number[] {
         conf.end = args[1]
         conf.step = args[2]
     }
-    console.log(`CONF READY:`, { ...conf, is_end_positive: Math.sign(conf.end)})
 
     const arrayItemsQauntity = conf.step === 0 ? conf.step : Math.ceil(Math.abs(conf.end) / Math.abs(conf.step))
-    console.log(`LENGTH [math :${Math.abs(conf.end) / Math.abs(conf.step)}]:`, arrayItemsQauntity)
 
     const empty: number[] = new Array(arrayItemsQauntity)
 
@@ -43,9 +38,6 @@ export function range(start?: number, end: number, step?: number): number[] {
     })
 
     const result = stepped.filter((item) => item !== undefined)
-
-    console.log(`RESULT [length :${result.length}]:`, result)
-    console.log('~~~~~~~~~~~~~~~~~~~~')
     return result
 }
 
